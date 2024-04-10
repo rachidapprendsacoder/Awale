27b9911210c31bdfbf57f5d8f39e71ba35422ed1
majidAzzou82 <152651524+majidAzzou82@users.noreply.github.com>
2024-04-10 22:11:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ screen = pygame.display.set_mode((640, 455))
 pygame.display.set_caption("Awalé 2.0")
 background = pygame.image.load(background_image).convert()
 
-
+last_posA = 0
+last_posB = 0
 nba, nbb = 24, 24
 plateau = [4,4,4,4,4,4,4,4,4,4,4,4]
 run = nba!=0 and nbb!=0
-
+tour = True
 def affichage():
     for i in range(6):
         pygame.draw.circle(background, (250, 250, 0), (70+i*100, 130), 35)
@@ -28,17 +29,52 @@ def repartition(num):
     while plateau[num]!=0:
         plateau[(num + i) % 12] += 1
         plateau[(num) % 12] -= 1
-        i+=1
-
-    print(plateau)
+        i=(i-1)%12
+    print(plateau[:6],plateau[12:5:-1])
 num = ''
 while run:
 
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+
+
+            if 34<pos[0]<105 :
+                if 95<pos[1]<165:
+                    repartition(0)
+                elif 296<pos[1]<366:
+                    repartition(11)
+            if 135<pos[0]<206 :
+                if 95<pos[1]<165:
+                    repartition(1)
+                elif 296<pos[1]<366:
+                    repartition(10)
+            if 235<pos[0]<306 :
+                if 95<pos[1]<165:
+                    repartition(2)
+                elif 296<pos[1]<366:
+                    repartition(9)
+            if 335<pos[0]<406 :
+                if 95<pos[1]<165:
+                    repartition(3)
+                elif 296<pos[1]<366:
+                    repartition(8)
+            if 435<pos[0]<506 :
+                if 95<pos[1]<165:
+                    repartition(4)
+                elif 296<pos[1]<366:
+                    repartition(7)
+            if 535<pos[0]<606 :
+                if 95<pos[1]<165:
+                    repartition(5)
+                elif 296<pos[1]<366:
+                    repartition(6)
+
 
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.KEYDOWN:
+        '''if event.type == pygame.KEYDOWN and len(str(num))<2:
 
             if event.key == pygame.K_1:
                 num=num+'1'
@@ -68,12 +104,12 @@ while run:
             elif event.key == pygame.K_RETURN:
                 print(num)
                 repartition(int(num))
-                num=''
+                num='''''
         affichage()
         screen.fill((0, 0, 0))
         screen.blit(background, (0, 0))
         pygame.display.update()
-
+    tour = not tour
 
 pygame.quit()
 quit()
