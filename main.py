83c6bd3146eecb232f5e4be477bc4032b38603fd
majidAzzou82 <152651524+majidAzzou82@users.noreply.github.com>
2024-04-13 17:36:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,41 @@ import pyxel
 import bot
 
 screen_size_x, screen_size_y = 300,200
+def repartition_sim(simulation, num):
+    i = 0
 
+    # Tant que la case où on puise les graines est pleine, on remplit
+    while simulation[0][num] != 0:
+        simulation[0][(num + i) % 12] += 1
+        simulation[0][(num) % 12] -= 1
+        i = (i - 1) % 12
+    if simulation[0][(num+i+1)%12] in [2,3]:
+        return recuperation_graines_sim((num+i+1)%12)
+    return simulation
+def legal_moves(simulation):
+    #donne une liste de bon coup pour le joueur concerné
+    return
+def in_game_sim(simulation,choix):
+    if choix is not None:
+        tour = simulation[1]
+        simulation[1] = not tour
+        if tour:  # Vérifie le tour du joueur
+            return repartition_sim(choix)
 
-
+        else:  # Vérifie le tour du joueur
+            return repartition_sim(11 - choix)
+def recuperation_graines_sim(self,simulation,num):
+        i = num
+        tour = simulation[1]
+        while simulation[0][i] in [2,3]:
+            if tour and 6<=i<=11:
+                simulation[2] += simulation[0]
+                simulation[0][i] = 0
+            elif not tour and 0<=i<=5:
+                simulation[3] += simulation[0]
+                simulation[0][i] = 0
+            i = (i+1)%12
+        return simulation
 class App:
     def __init__(self):
         pyxel.init(screen_size_x, screen_size_y)
@@ -92,6 +124,7 @@ class App:
             elif not self.tour:  # Vérifie le tour du joueur
                 self.repartition(11 - choix)
             self.tour = not self.tour
+
     def get_game(self):
         return self.plateau_visu, self.tour, self.scoreA,self.scoreB
     def init_button(self):
@@ -104,7 +137,7 @@ class App:
             self.nourrissage()
 
             if self.tour:
-                self.in_game(bot.bot_random_move(self.get_game()))
+                self.in_game(bot.bot_move(self.get_game()))
             else:
                 self.in_game(self.player_control())
 
@@ -141,10 +174,15 @@ class App:
                 pyxel.blt(26+i*40,40+70*j,0,0,0,32,32,0)
                 pyxel.text(40+i*40,54+70*j,str(self.plateau_visu[j][i]),8)
 
+
+        print(self.last_posB)
         pyxel.blt(26+self.last_posA*40,40,0,32,0,32,32,0)
-        pyxel.text(40+self.last_posA*40,54,str(self.plateau_visu[1][self.last_posA]),8)
         pyxel.blt(26+self.last_posB*40,50+60,0,32,0,32,32,0)
-        pyxel.text(40+self.last_posB*40,54+70,str(self.plateau_visu[1][self.last_posB]),8)
+        pyxel.text(40+self.last_posA*40,54,str(self.plateau_visu[0][self.last_posA]),8)
+        pyxel.text(40+self.last_posB*40,70+54,str(self.plateau_visu[1][self.last_posB]),8)
+        '''#pyxel.text(40+self.last_posA*40,54,str(self.plateau_visu[1][self.last_posA]),8)
+
+        #pyxel.text(40+(self.last_posB-1)*40,54+70,str(self.plateau_visu[1][self.last_posB]),8)'''
 
         pyxel.text(10,10,'Reset',7)
         if 10<pyxel.mouse_x<30 and 10<pyxel.mouse_y<15:
@@ -155,9 +193,6 @@ class App:
                 pyxel.text(110,80,"Le Robot a gagne !",7)
             else:
                 pyxel.text(110, 80, "Le Joueur a gagne !",7)
-
-
-print(bot.bot_random_move(0))
 
 jeu = App()
 jeu.start()
